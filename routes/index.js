3af84964fea1a063f6b7646f9db595b9066389c7
Yeison Garzón T <toxiicsa@hotmail.com>
2021-02-26 02:32:29 -0500

    ADD: routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const router = require("express").Router();
+
+const homeRoutes = require("./home.routes");
+const {ErrorMiddleware, NotFoundMiddleware, /* ParseIntMiddleware */} = require('../middlewares');
+
+router.use('/home', homeRoutes);
+
+router.use(ErrorMiddleware);
+router.use(NotFoundMiddleware);
+
+module.exports = router;
