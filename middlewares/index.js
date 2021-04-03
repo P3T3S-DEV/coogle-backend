7006a3bcbb30436823a55ebcfba93188c28aafee
Yeison Garzón T <toxiicsa@hotmail.com>
2021-04-03 02:44:24 -0500

    ADD: auth middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     NotFoundMiddleware: require('./not-found.middleware'),
     ErrorMiddleware: require('./error.middleware'),
-    ParseIntMiddleware: require('./parse-int.middleware')
+    ParseIntMiddleware: require('./parse-int.middleware'),
+    AuthMiddleware: require('./auth.middleware')
 }
