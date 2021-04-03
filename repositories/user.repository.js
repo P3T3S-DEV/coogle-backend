dda21677f3f6b1edb440c487522fd685b622bc80
Yeison Garzón T <toxiicsa@hotmail.com>
2021-04-03 02:44:40 -0500

    ADD: functions to user repository

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,10 +1,19 @@
 "use strict";
 const userModel = require("../models/user.model");
 
+const getUserById = async (userId) =>{
+  return await userModel.findById(userId);
+}
 const getUserByUsername = async (username) => {
-  return await userModel.findOne({ username });
+  return await userModel.findOne({ username }, "-categories -notes");
+};
+
+const updateUser = async (id, user) => {
+  return await userModel.findByIdAndUpdate(id, user, { new: true });
 };
 
 module.exports = {
-  getUserByUsername,
+  getUserById,
+  updateUser,
+  getUserByUsername
 };
