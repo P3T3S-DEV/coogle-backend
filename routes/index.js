48675c7899fe71fb0c39dcbd7224d775993f0555
Yeison Garzón T <toxiicsa@hotmail.com>
2021-04-03 02:43:34 -0500

    ADD: note use case

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = require("express").Router();
 
 const homeRoutes = require("./home.routes");
 const authRoutes = require("./auth.routes");
+const noteRoutes = require("./note.routes");
+
 const {
   ErrorMiddleware,
   NotFoundMiddleware /* ParseIntMiddleware */,
@@ -11,6 +13,7 @@ const {
 
 router.use("/home", homeRoutes);
 router.use("/auth", authRoutes);
+router.use("/note", noteRoutes);
 
 router.use(ErrorMiddleware);
 router.use(NotFoundMiddleware);
